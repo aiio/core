ec898c2c3f1f9e5868053ed3fb77c3d09bea515b
cnbattle <qiaicn@gmail.com>
2022-06-12 15:20:07 +0800

    gift

--- modelx/configs.go
+++ modelx/configs.go
@@ -9,6 +9,7 @@ import (
 // ConfigGroup 配置分组
 type ConfigGroup struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
+	Owner     string         `gorm:"type:varchar(128);default:'';comment:所属者" json:"owner"`
 	Name      string         `gorm:"type:varchar(128);default:'';comment:分组名称" json:"name"`
 	Tag       string         `gorm:"type:char(32);default:'';uniqueIndex;comment:分组唯一tag" json:"tag"`
 	Remark    string         `gorm:"type:varchar(128);default:'';comment:备注" json:"remark"`
@@ -16,12 +17,13 @@ type ConfigGroup struct {
 	Status    int            `gorm:"type:tinyint(4);default:0;comment:状态：0禁用，1启用" json:"status"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 // Config 配置信息
 type Config struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
+	Owner     string         `gorm:"type:varchar(128);default:'';comment:所属者" json:"owner"`
 	GroupTag  string         `gorm:"type:char(32);default:'';comment:分组标签" json:"group_tag"`
 	Tag       string         `gorm:"type:char(32);default:'';uniqueIndex;comment:分组唯一tag" json:"tag"`
 	Value     string         `gorm:"type:varchar(1024);default:'';comment:值" json:"value"`
@@ -30,5 +32,5 @@ type Config struct {
 	Status    int            `gorm:"type:tinyint(4);default:0;comment:状态：0禁用，1启用" json:"status"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
